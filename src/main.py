c13a182fc1437375b910f0952a8a7f3afbff6214
tomasfanciotti <tomasfanciotti@gmail.com>
2021-09-23 23:40:50 -0300

    Base de datos y procfile actualizados

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
-import uvicorn
 from src.schemas import CursoSchema
 from sqlalchemy.orm import Session
 from src.db.database import get_db
 from src.db.crud import crud_curso
 
-api = FastAPI()
+app = FastAPI()
 
 
-@api.get('/')
+@app.get('/')
 async def index():
     return {
         "message": "Hello World!",
@@ -16,17 +15,17 @@ async def index():
     }
 
 
-@api.post('/curso')
+@app.post('/curso')
 def crear_curso(curso: CursoSchema.CursoBase):
     return curso
 
 
-@api.post("/cursos/", response_model=CursoSchema.Curso)
+@app.post("/cursos/", response_model=CursoSchema.Curso)
 def create_curso(curso: CursoSchema.CursoCreate, db: Session = Depends(get_db)):
     return crud_curso.create_curso(db=db, curso=curso)
 
 
-@api.get("/cursos/{curso_id}", response_model=CursoSchema.Curso)
+@app.get("/cursos/{curso_id}", response_model=CursoSchema.Curso)
 def read_user(curso_id: int, db: Session = Depends(get_db)):
     db_user = crud_curso.get_curso(db, curso_id=curso_id)
     if db_user is None:
@@ -34,11 +33,10 @@ def read_user(curso_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@api.get('/users')
+@app.get('/users')
 def get_hardcoded_user():
     return {
         "user": "pedro123!",
         "password": "123456"
     }
 
-uvicorn.run(api)
