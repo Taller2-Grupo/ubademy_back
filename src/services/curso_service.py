653710e4ad394d48e416191e8955c058e6f7fb92
lituarte <lautaro.javier.ituarte@gmail.com>
2021-10-05 21:51:10 -0300

    Fix get cursos sin filtrar por ningun estado

--- src/services/curso_service.py
+++ src/services/curso_service.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException
 from src.schemas import CursoSchema
 from src.models.CursoModel import EstadoCursoEnum
-from typing import List
+from typing import List, Optional
 
 
 def crear_curso(curso: CursoSchema.CreateCursoRequest, db: Session):
@@ -25,5 +25,5 @@ def eliminar_curso(curso_id: uuid.UUID, db: Session):
     return curso_repository.actualizar_curso(db, db_curso)
 
 
-def get_cursos(estados: List[EstadoCursoEnum], db: Session):
+def get_cursos(estados: Optional[List[EstadoCursoEnum]], db: Session):
     return curso_repository.get_curso_by_estados(estados, db)
