d300690b1acf8195bafb94e00d4d10adf563daa5
lituarte <lautaro.javier.ituarte@gmail.com>
2021-10-06 18:08:24 -0300

    Permito todos los origenes

--- src/main.py
+++ src/main.py
@@ -12,14 +12,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
