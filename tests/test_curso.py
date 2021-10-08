050e69717e71ff5fbd26c9abffbb4f965341fb90
RenzoJacinto <32022609+RenzoJacinto@users.noreply.github.com>
2021-10-08 16:10:56 -0300

    agrego test main

--- tests/test_curso.py
+++ tests/test_curso.py
@@ -1,15 +1,9 @@
 from unittest import TestCase
-from src.db.database import get_db
+from src.db.database import get_db, Base
 from src.models.CursoModel import Curso
 from src.schemas import CursoSchema
 from src.schemas.CursoSchema import CreateCursoRequest
 from src.services.curso_service import crear_curso
-from fastapi.testclient import TestClient
-from src.main import app
-
-
-testClient = TestClient(app)
-
 
 class CursoTest(TestCase):
 
@@ -34,7 +28,5 @@ class CursoTest(TestCase):
         curso.eliminar()
         self.assertTrue(curso.estado == 'eliminado')
 
-    def test(self):
-        response = testClient.get("/cursos")
-        assert response.status_code == 200
+
 
---
+++ tests/test_main.py
@@ -0,0 +1,59 @@
+from unittest import TestCase
+from fastapi.testclient import TestClient
+from src.main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://szbbimktemtmxp:abb783d6b84c1f607061d64dacb901af10cae40d24a2b6d96110d7efe477b5c4@ec2-54-174-172-218.compute-1.amazonaws.com:5432/d3vhnlg19iblmb"
+#
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL
+# )
+# TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#
+#
+# Base.metadata.create_all(bind=engine)
+#
+#
+# def override_get_db():
+#     try:
+#         db = TestingSessionLocal()
+#         yield db
+#     finally:
+#         db.close()
+#
+#
+# app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
+
+class MainTest(TestCase):
+
+    def testPostCurso(self):
+        response = client.post('/cursos/',
+                               json={'id_creador': 'dadb4e2f-63d1-45d4-9f44-2d68a07105cc', 'titulo': 'postCurso'
+                                   , 'descripcion': 'descr'})
+        assert response.status_code == 200
+
+    def testGetCursos(self):
+        response = client.get('/cursos')
+        assert response.status_code == 200
+
+    def testGetCurso(self):
+        response_post = client.post('/cursos/',
+                                    json={'id_creador': '615e3022-fd6a-4fd0-ba8b-0b8a3549a067', 'titulo': 'GetCurso',
+                                          'descripcion': 'descr'})
+        id_post = response_post.json().get('id')
+        print(id_post)
+        response = client.get('/cursos/' + id_post)
+        print(response.status_code)
+        assert response.status_code == 200
+
+    def testDeleteCurso(self):
+        response_post = client.post('/cursos/',
+                                    json={'id_creador': 'fa3333cf-10e2-44df-9bc5-ae4c8d936c66', 'titulo': 'DeleteCurso',
+                                          'descripcion': 'descr'})
+        id_post = response_post.json().get('id')
+        response = client.delete('/cursos/' + id_post)
+        print(response.status_code)
+        assert response.status_code == 200
